23e00ff817e474abb65e75d76af99fd61f1f0b89
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-03 23:49:20 +0800

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(oldArr, ...elements) {
+    if (arguments.length < 2) return oldArr;
+    const newArr = [];
+    outer: 
+    for (const item of oldArr) {
+        for (const element of elements) {
+            if (item === element) continue outer;
+        }
+        newArr.push(item);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
