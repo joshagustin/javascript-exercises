ee87b1f2a84e7d3bb516d1d38a5a41f4ee3cffb0
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-03 23:24:13 +0800

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringIndex = string.length - 1;
+    let reversedString = '';
+    for (let i = stringIndex; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
