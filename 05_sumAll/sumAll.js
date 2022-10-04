ae9cc84106b7c1786ad4ff221231a9bf9d05566e
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-04 19:12:35 +0800

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (isNotValid(firstNum, secondNum)) return 'ERROR';
+    let sum = 0;
+    let startNum = firstNum < secondNum ? firstNum : secondNum;
+    let endNum = firstNum > secondNum ? firstNum : secondNum;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+function isNotValid(firstNum, secondNum) {
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return true;
+    }
+    else if (firstNum < 0 || secondNum < 0) {
+        return true;
+    }
+    return false;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
