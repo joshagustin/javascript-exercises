78459c2dc299085534776c943a545f6ca35631fb
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-27 14:29:55 +0800

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let num = Number(index);
+    if (num < 0) return "OOPS";
+    let first = 0, last = 1;
+    for (let i = 1; i < num; i++) {
+        let tmp = last;
+        last = first + last;
+        first = tmp;
+    }
+    return last;
 };
 
 // Do not edit below this line
