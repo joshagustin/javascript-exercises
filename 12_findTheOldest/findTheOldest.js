2f599d1e95426c0cbf6a5f12a334855340a9527e
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-27 14:44:44 +0800

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    // sorts descending
+    const oldestSorted = arr.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        const aAge = getAge(a);
+        const bAge = getAge(b)
+        return aAge > bAge ? -1 : 1;
+    });
+    return oldestSorted[0];
 };
+
+function getAge(obj) {
+    let finalYear = obj.yearOfDeath
+    if (!finalYear) {
+        finalYear = new Date().getFullYear();
+    }
+    return finalYear - obj.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
