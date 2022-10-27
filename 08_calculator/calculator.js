536daa8457139b290837bb4fb6ebe0185e8f7060
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-27 13:42:46 +0800

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const tmp = arr.reduce((total, item) => total += item, 0);
+  return tmp;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  const tmp = arr.reduce((total, item) => total *= item, 1);
+  return tmp;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let tmp = 1;
+  for (let i = a; i > 0; i--) {
+    tmp *= i;
+  }
+  return tmp;
 };
 
 // Do not edit below this line
