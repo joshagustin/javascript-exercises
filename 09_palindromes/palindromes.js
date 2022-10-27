28531f32e16cd36dc8aa6c7ae240c5279b8fba2f
Joshua Agustin <joshuadavide.agustinjdea@gmail.com>
2022-10-27 14:11:00 +0800

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let tmp = str.split(/[\s,!.]+/).join('');
+    let length = tmp.length;
+    for (let head = 0, tail = length - 1; head < tail; head++, tail--) {
+        if (tmp[head].toLowerCase() != tmp[tail].toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
